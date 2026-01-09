1b4d40dca689ca992191b44b9f472e1ea61ac74a
Kayley Head <120121645+headk@users.noreply.github.com>
2026-01-09 11:47:51 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,1 +1,11 @@
+# global.R
+source("R/packages.R")
+source("R/paths.R")
+
+source("R/helpers.R")
+
+source("R/data_polygons.R")
+source("R/data_occurrences.R")
+source("R/data_grids.R")
+source("R/data_sara_ais.R")
 
